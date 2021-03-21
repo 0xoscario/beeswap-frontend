aca2f91ac18ac0633e423f7583100e4a08a25394
Oscar Buck <buckoscarengr@gmail.com>
2021-03-21 14:54:52 -0700

    Removed Risk

--- src/views/Farms/components/FarmCard/CardHeading.tsx
+++ src/views/Farms/components/FarmCard/CardHeading.tsx
@@ -35,7 +35,7 @@ const CardHeading: React.FC<ExpandableSectionProps> = ({
         <Heading mb="4px">{lpLabel}</Heading>
         <Flex justifyContent="center">
           {/* {isCommunityFarm ? <CommunityTag /> : <CoreTag />} */}
-          <RiskTag risk={risk} />
+          {/* <RiskTag risk={risk} /> */}
           <MultiplierTag variant="secondary">{multiplier}</MultiplierTag>
         </Flex>
       </Flex>
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -59,9 +59,9 @@ const Home: React.FC = () => {
     <Page>
       <Hero>
         <Heading as="h1" size="xl" mb="24px" color="secondary">
-          {TranslateString(576, 'Goose Finance')}
+          {TranslateString(576, 'BeeSwap')}
         </Heading>
-        <Text>{TranslateString(578, 'The #1 AMM and yield farm on Binance Smart Chain.')}</Text>
+        <Text>{TranslateString(578, 'The Sweetest AMM and yield farm on Binance Smart Chain')}</Text>
       </Hero>
       <div>
         <Cards>
