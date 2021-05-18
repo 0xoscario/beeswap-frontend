471ea080c5f8f8a03345768223431a7bed063c15
Oscar Buck <buckoscarengr@gmail.com>
2021-05-17 19:06:42 -0700

    Changed LP links to beeswap

--- src/views/Farms/components/FarmCard/DetailsSection.tsx
+++ src/views/Farms/components/FarmCard/DetailsSection.tsx
@@ -50,7 +50,7 @@ const DetailsSection: React.FC<ExpandableSectionProps> = ({
     <Wrapper>
       <Flex justifyContent="space-between">
         <Text>{TranslateString(316, 'Stake')}:</Text>
-        <StyledLinkExternal href={`https://exchange.pancakeswap.finance/#/add/${liquidityUrlPathParts}`}>
+        <StyledLinkExternal href={`https://exchange.beeswap.finance/#/add/${liquidityUrlPathParts}`}>
           {lpLabel}
         </StyledLinkExternal>
       </Flex>
